631aa682ee0bfe845697b5723adb4da797393e20
keerthi-2818 <keerthikoratla@gmail.com>
2026-06-13 14:56:17 +0530

    add YOLO object detection project files

--- Code_Mentor_Ai-main/create_db.py
+++
@@ -1,8 +0,0 @@
-from app.run import app
-from app import db
-# Import all models so SQLAlchemy knows about them
-from app.models import User_details, Snippet, Challenge, UserChallengeProgress
-
-with app.app_context():
-    db.create_all()
-    print("Database tables created!")
--- Code_Mentor_Ai-main/seed.py
+++
@@ -1,3 +0,0 @@
-from app.seed_challenges import seed_challenges
-from app.run import app
-seed_challenges(app)
